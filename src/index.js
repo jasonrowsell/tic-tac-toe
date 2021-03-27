3a4fb7754bdaa80d12b49e934b125e86f0933381
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-27 13:37:07 +0000

    updated typos

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ class Game extends React.Component {
   render() {
     return (
       <div className="game">
-        <div className="game-baord">
+        <div className="game-board">
           <Board />
         </div>
         <div className="game-info">
